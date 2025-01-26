ae8c0325a0439308d6cca69519cecddcd8b2b1d3
jakemackie <jake.wmackie@gmail.com>
2025-01-26 11:55:13 +0000

    feat: Fixed sidebar

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,7 +1,19 @@
+import { SidebarProvider } from '@/components/ui/sidebar';
+import { AppSidebar } from '@/components/global/navigation/sidebar';
+import { Toaster } from '@/components/ui/sonner';
+
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="flex-1 my-2 mx-2 md:ml-0 p-4 rounded-lg bg-border border">
+        {children}
+        <Toaster />
+      </main>
+    </SidebarProvider>
+  );
 }
---
+++ src/components/global/navigation/sidebar.tsx
@@ -0,0 +1,126 @@
+import {
+  SidebarProvider,
+  Sidebar,
+  SidebarHeader,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarFooter,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+} from '@/components/ui/sidebar';
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '@/components/ui/collapsible';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+import {
+  ChevronUp,
+  ChevronDown,
+  MessageSquareText as FAQ,
+  Send as Feedback,
+  User2,
+  Home,
+  Settings,
+} from 'lucide-react';
+
+export function AppSidebar() {
+  const topLevelNavItems = [
+    { title: 'Home', url: '/dashboard', icon: Home },
+    { title: 'Q&A', url: '/dashboard/faq', icon: FAQ },
+  ];
+
+  const helpNavItems = [
+    { title: 'Feedback', url: '/dashboard/feedback', icon: Feedback },
+  ];
+
+  return (
+    <Sidebar variant="floating">
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {topLevelNavItems.map((navItem) => (
+                <SidebarMenuItem key={navItem.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={navItem.url}>
+                      <navItem.icon />
+                      <span>{navItem.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <Collapsible defaultOpen className="group/collapsible">
+          <SidebarGroup>
+            <SidebarGroupLabel asChild>
+              <CollapsibleTrigger>
+                Help
+                <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+              </CollapsibleTrigger>
+            </SidebarGroupLabel>
+            <CollapsibleContent>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  {helpNavItems.map((navItem) => (
+                    <SidebarMenuItem key={navItem.title}>
+                      <SidebarMenuButton asChild>
+                        <a href={navItem.url}>
+                          <navItem.icon />
+                          <span>{navItem.title}</span>
+                        </a>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  ))}
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </CollapsibleContent>
+          </SidebarGroup>
+        </Collapsible>
+      </SidebarContent>
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  <User2 /> Username
+                  <ChevronUp className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent
+                side="top"
+                className="w-[--radix-popper-anchor-width]"
+              >
+                <DropdownMenuItem>
+                  <span>Account</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Billing</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Sign out</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
