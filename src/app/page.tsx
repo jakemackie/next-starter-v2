c6dac1f99fd5e8c55cd51aa00f6b603066f788e6
Jake Mackie <jake@hiyield.co.uk>
2025-01-30 07:48:49 +0000

    feat: Gitkeep some pages I plan to make

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import { prisma } from '@/lib/prisma';
 import { Button } from '@/components/ui/button';
 
 export default async function Home() {
-  const temp = await prisma.temp.findMany();
   return (
     <div>
       <h1>Next starter</h1>
       <Button>Click me</Button>
-      <pre>{JSON.stringify(temp, null, 2)}</pre>
     </div>
   );
 }
